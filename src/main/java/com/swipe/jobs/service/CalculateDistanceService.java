c123b22580f2af5bbe80a94647f9818409bdb93e
Kaushsi <shaily.kaushik@gmail.com>
2017-08-29 21:41:46 +1000

    delete files-not needed

--- src/main/java/com/swipe/jobs/service/CalculateDistanceService.java
+++
@@ -1,7 +0,0 @@
-package com.swipe.jobs.service;
-
-public interface CalculateDistanceService {
-	
-	Double getDistance(Double longitudeFrom, Double latitudeFrom, Double longitudeTo, Double latitudeTo);
-
-}
